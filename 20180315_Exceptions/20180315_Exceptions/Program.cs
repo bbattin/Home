5d24debdf3dd28478b1b622bc2b74c3be5ae9397
Battin <batttin@hotmail.com>
2018-03-16 17:33:00 +0200

    ..

--- 20180315_Exceptions/20180315_Exceptions/Program.cs
+++ 20180315_Exceptions/20180315_Exceptions/Program.cs
@@ -10,6 +10,23 @@ namespace _20180315_Exceptions
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("a*x*x + b*x + c = 0");
+            int a = GetNumberByUser();
+            int b = GetNumberByUser();
+            int c = GetNumberByUser();
+
+            SquareEquationSolver result = new SquareEquationSolver(a, b, c);
+            result.Calculate();
+            Console.WriteLine("D = {0}, x1 = {1}, x2 = {2}", result.D, result.Root1, result.Root2);
+
+            Console.ReadKey();
+        }
+
+        private static int GetNumberByUser()
+        {
+            Console.WriteLine("Enter number = ");
+            int a = int.Parse(Console.ReadLine());
+            return a;
         }
     }
 }
