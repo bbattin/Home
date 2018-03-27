a73bbc924bbe871e557326c81f5f89f7d69ddc59
Battin <batttin@hotmail.com>
2018-03-27 11:05:47 +0300

    ......

--- 20180325_Events/20180325_Events/Analizer.cs
+++ 20180325_Events/20180325_Events/Analizer.cs
@@ -8,5 +8,22 @@ namespace _20180325_Events
 {
     class Analizer
     {
+        public Analizer(Sorter p)
+        {
+            p.Compare += OnNextCompare1;
+            p.Compare += OnNextCompare2;
+        }
+
+        public void OnNextCompare1(object sender, CompareEventArgs args)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("Analizer.OnNextCompare1(): args.FirstNumber = {0}, args.SecondNumber = {1}", args.FirstNumber, args.SecondNumber);
+        }
+
+        public void OnNextCompare2(object sender, CompareEventArgs args)
+        {
+            Console.ForegroundColor = ConsoleColor.Magenta;
+            Console.WriteLine("Analizer.OnNextCompare2(): args.FirstNumber = {0}, args.SecondNumber = {1}", args.FirstNumber, args.SecondNumber);
+        }
     }
 }
