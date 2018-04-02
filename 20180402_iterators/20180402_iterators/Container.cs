d550d5408cffc13e7a502fb81adfb03d3b75e2bc
Battin <batttin@hotmail.com>
2018-04-02 16:47:35 +0300

    iterators

---
+++ 20180402_iterators/20180402_iterators/Container.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _20180402_iterators
+{
+    class Container
+    {
+        public Container(int size)
+        {
+            _items = new object[size];
+        }
+
+        public object this[int index]
+        {
+            get
+            {
+                return _items[index];
+            }
+            set
+            {
+                if (_items[index] != null)
+                {
+                    _items[index] = value;
+                }
+            }
+        }
+
+        public int Count
+        {
+            get
+            {
+                return _count;
+            }
+        }
+
+        public void Add(object item)
+        {
+            _items[_count++] = item;
+        }
+
+        private int _count = 0;
+        private object[] _items;    // элементы контейнера
+    }
+}
---
+++ 20180402_iterators/20180402_iterators/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _20180402_iterators
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack s = new Stack();
+            s.Push(1);
+            s.Push(10);
+            s.Push(100);
+            Console.WriteLine(s.Pop());
+            Console.WriteLine(s.Pop());
+            Console.WriteLine(s.Pop());
+
+
+            // ContainerDemo();
+
+            Console.ReadKey();
+        }
+
+        private static void ContainerDemo()
+        {
+            Container c = new Container(10);
+
+            c.Add(2);
+            c.Add(5);
+            c.Add(-5);
+            c.Add(8);
+
+            PrintContainer2(c);
+
+            c.Add(2.01);
+
+            PrintContainer2(c);
+        }
+
+        // with unbpoxing
+        private static void PrintContainer1(Container c)
+        {
+            for (int i = 0; i < c.Count; i++)
+            {
+                int item = (int)c[i];
+                Console.Write("{0}\t", item);
+            }
+            Console.WriteLine();
+        }
+
+        // without unbpoxing
+        private static void PrintContainer2(Container c)
+        {
+            for (int i = 0; i < c.Count; i++)
+            {
+                //string item = (string)c[i];
+                string item = c[i].ToString();
+
+                int number = int.Parse(item);
+
+                Console.Write("{0}\t", number);
+
+                //Console.Write("{0}\t", c[i]);
+            }
+            Console.WriteLine();
+        }
+    }
+}
