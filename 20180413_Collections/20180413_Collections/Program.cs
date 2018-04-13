697779e1ca30814dbd3e8d8577abb3e90b295bf0
Battin <batttin@hotmail.com>
2018-04-13 17:07:13 +0300

    + SortedList

--- 20180413_Collections/20180413_Collections/Program.cs
+++ 20180413_Collections/20180413_Collections/Program.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace _20180413_Collections
+namespace LinkedList
 {
     class Program
     {
---
+++ 20180413_Collections/SortedList/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SortedList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Создадим коллекцию сортированного списка
+
+            SortedList<string, string> UserInfo = new SortedList<string, string>();
+
+            // Добавим несколько элементов в коллекию
+            UserInfo.Add("Zack", "12345");
+            UserInfo.Add("Den", "12345");
+            UserInfo.Add("Alex", "12345");
+            UserInfo.Add("John", "12345");
+            UserInfo.Add("Elhm", "12345");
+            UserInfo.Add("Lamar", "12345");
+
+
+
+            // Коллекция ключей
+            ICollection<string> keys = UserInfo.Keys;
+
+            // Теперь используем ключи, для получения значений
+            foreach (string s in keys)
+                Console.WriteLine("User: {0}, Password: {1}", s, UserInfo[s]);
+
+            Console.ReadLine();
+        }
+    }
+}
