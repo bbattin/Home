9e386ac8c21e83f7d5736da094909aeb1aeb67e9
Battin <batttin@hotmail.com>
2018-04-20 16:43:45 +0300

    +

---
+++ 20180420_hubspot.com/20180420_hubspot.com/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace _20180420_hubspot.com
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string url = "https://api.hubapi.com/contacts/v1/lists/recently_updated/contacts/recent?hapikey=demo&count=2";
+            WebClient client = new WebClient();
+            var result = client.DownloadString(url);
+            Console.Write(result);
+            Console.ReadKey();
+        }
+    }
+}
