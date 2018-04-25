1c765cdd0c9b7a4e039be8e7fc69e6cbb2a6e07d
Battin <batttin@hotmail.com>
2018-04-25 16:12:21 +0300

    CreateCSVFile

--- 20180420_hubspot.com/20180420_hubspot.com/Program.cs
+++ 20180420_hubspot.com/20180420_hubspot.com/Program.cs
@@ -7,6 +7,7 @@ using System.Net;
 using System.Threading.Tasks;
 using Excel = Microsoft.Office.Interop.Excel;
 using System.Reflection;
+using System.IO;
 
 namespace _20180420_hubspot.com
 {
@@ -14,45 +15,57 @@ namespace _20180420_hubspot.com
     {
         static void Main(string[] args)
         {
-            string hapikey = "demo";
-            string countContacts = "10";
             string timeOffset = "1524480259611";
-            string vidOffset = "5875024";
-            string url = "https://api.hubapi.com/contacts/v1/lists/recently_updated/contacts/recent?hapikey=" + hapikey + "&count=" + countContacts + "&timeOffset=" + timeOffset + "&vidOffset=" + vidOffset;
-            WebClient client = new WebClient();
-            string result = client.DownloadString(url);
+            string result = GetContactList(timeOffset);
             Console.Write(result);
-        
+
             ResponseJson responseJson = JsonConvert.DeserializeObject<ResponseJson>(result);
             Console.WriteLine();
-            
+
             Console.WriteLine(responseJson.Responses.Count);
 
-            writingInExcMethod();
+            CreateCSVFile(responseJson);
 
             Console.ReadKey();
         }
 
-        private static void writingInExcMethod()
+        private static void CreateCSVFile(ResponseJson responseJson)
         {
-            try
+            StreamWriter csv = new StreamWriter("test.csv", true, Encoding.GetEncoding(1251));   // Win-кодировка
+            csv.WriteLine("Vid; Firstname; Lastname; Lastmodifieddate; Company; PortalId");      // заголовок
+
+
+            foreach (Response p in responseJson.Responses)
             {
-                Excel.Application ObjExcel = new Excel.Application();
-                Excel.Workbook ObjWorkBook;
-                Excel.Worksheet ObjWorkSheet;
-                ObjWorkBook = ObjExcel.Workbooks.Add(Missing.Value);
-                ObjWorkSheet = (Excel.Worksheet)ObjWorkBook.Sheets[1];
-                ObjWorkSheet.Cells[3, 1] = "51";
-                ObjWorkBook.SaveAs("log.xlsx", Missing.Value, Missing.Value, Missing.Value, Missing.Value, Missing.Value, Excel.XlSaveAsAccessMode.xlExclusive,
-                Missing.Value, Missing.Value, Missing.Value, Missing.Value, Missing.Value);
-                
-                ObjExcel.Quit();
+                csv.WriteLine(ChekAndGetParametr(p.Vid) + ";" + ChekAndGetParametr(p.Properties.Firstname) + ";" 
+                    + ChekAndGetParametr(p.Properties.Lastname) + ";" + ChekAndGetParametr(p.Properties.Lastmodifieddate) 
+                    + ";" + ChekAndGetParametr(p.Properties.Company) + ";" + ChekAndGetParametr(p.PortalId));   
             }
-            catch (Exception exc)
-            {
-                Console.WriteLine("Ошибка при составлении лога\n" + exc.Message);
-            }
+
+            csv.Close();
         }
 
+        private static string ChekAndGetParametr(object a)
+        {
+            if (a == null)
+            {
+                //a.ToString();
+                a = "";
+            }
+            return a.ToString();
+        }
+
+        private static string GetContactList(string timeOffset)
+        {
+            string hapikey = "demo";
+            string countContacts = "10";
+            string vidOffset = "5875024";
+            string url = "https://api.hubapi.com/contacts/v1/lists/recently_updated/contacts/recent?hapikey=" + hapikey + "&count=" + countContacts + "&timeOffset=" + timeOffset + "&vidOffset=" + vidOffset;
+            WebClient client = new WebClient();
+            string result = client.DownloadString(url);
+            return result;
+        }
+
+       
     }
 }
